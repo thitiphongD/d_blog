c8f298d4769bf1aeceb59f6e20d53bb33dcb45cf
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-12-11 14:25:14 +0700

    fix ui

--- src/app/components/WorkExperience.tsx
+++ src/app/components/WorkExperience.tsx
@@ -9,16 +9,16 @@ const WorkExperience = () => {
         <ScrollArea className="md:h-[80vh]">
             {dataExperience.map((experience) => (
                 <Link key={experience.id}
-                    className="gap-2 py-2 flex flex-col md:flex-row mb-4 cursor-pointer hover:bg-white/10 hover:backdrop-blur-lg hover:brightness-90 group w-full"
+                    className=" md:p-4 flex flex-col md:flex-row mb-10 cursor-pointer hover:bg-white/10 hover:backdrop-blur-lg hover:brightness-90 group w-full rounded-lg"
                     href={experience.link}
                 >
                     <div className='w-full md:w-1/4'>
-                        <p className="text-xs font-bold">{experience.date}</p>
+                        <p className="text-xs font-bold mt-2">{experience.date}</p>
                     </div>
                     <div className='w-full md:w-3/4'>
-                        <p className="text-xl leading-normal font-semibold group-hover:text-[#3ecf8e]">{experience.position}, {experience.company}</p>
+                        <p className="mb-2 text-2xl leading-normal font-semibold group-hover:text-[#3ecf8e]">{experience.position}, {experience.company}</p>
                         <p className='tracking-wide leading-normal'>{experience.description}</p>
-                        <div className="mt-2 flex gap-2 flex-wrap">
+                        <div className="mt-4 flex gap-2 flex-wrap">
                             {experience.stack.map((tech, index) => (
                                 <Badge key={index} className="text-xs bg-[#3ecf8e]">
                                     {tech}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,15 +2,14 @@
 
 import React from 'react'
 import SwitchDarkMode from './SwitchDarkMode'
-import { Github } from 'lucide-react'
+import { Github, Linkedin } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import SearchComponent from './SearchComponent';
 
 const Navbar = () => {
-
     return (
-        <header className='container mx-auto px-4 md:px-10 py-6 flex justify-end items-center w-full'>
+        <header className='px-4 py-6 md:px-10 flex justify-end items-center w-full'>
             <div className='flex items-center space-x-1'>
                 <SearchComponent />
                 <Button asChild variant={'ghost'}>
@@ -18,10 +17,13 @@ const Navbar = () => {
                         <Github />
                     </Link>
                 </Button>
+                <Button asChild variant={'ghost'}>
+                    <Link href='https://www.linkedin.com/in/thitiphong-srisavat-36b264239' aria-label="Visit Thitiphong's Linkedin">
+                        <Linkedin />
+                    </Link>
+                </Button>
                 <SwitchDarkMode />
             </div>
-
-
         </header>
     )
 }
